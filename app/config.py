d802d1824c37abc629a69b6441209f55d32137ce
Dipanwita <susiejojo1@gmail.com>
2020-06-23 07:02:02 +0530

    [Addition] Heroku deployment1

---
+++ app/config.py
@@ -0,0 +1,8 @@
+ACCTOKEN="EAADyG75VvBMBAHIPFVC6kFsTSHuZB5wuGCF19Vpuou466ZCLTDC2oLtoeBTOZBiUX6LVG0J0ifZBGEGYd0ZCHPWhLyMkWsIKGxPwcYuRJXGMEuSnjtpCCspuMZA0ZCbBZC5c14bqAioZA50SZADE1cutpRiBlWE2vKLZCg6Gmn6jQ6S09N24aYO9mh5"
+VERTOKEN="susiejojoamy1999"
+DB_URL = "mongodb+srv://susiejojo1:qxjd9qB7XtJeODaU@cluster0-tapb2.mongodb.net/friend_indeed?retryWrites=true&w=majority"
+WIT_INTENT = "KTCKKIIDEILOOG6GDZ3TMPC6BIY2GYTX"
+WIT_DEPRESS = "N7OG7HJ4UAPOECYQ5WLIZXKOH5NSEYEZ"
+client_id = '4mzbizZ_uERM6Q'
+client_secret = 'oZFk5R4YZLEex_kiiQVvLTl7CnY'
+user_agent = 'meme-holder'
